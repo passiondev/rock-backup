de9c39477ee7ef06e6ec9cf7c077934e9decea4e
Daniel Hazelbaker <daniel@triumph.tech>
2022-01-27 08:30:31 -0700

    - Fixed rockFormField component to clear any form errors it has generated when it is removed from the DOM.

--- Rock.JavaScript.Obsidian/Framework/Elements/rockFormField.ts
+++ Rock.JavaScript.Obsidian/Framework/Elements/rockFormField.ts
@@ -15,7 +15,7 @@
 // </copyright>
 //
 
-import { computed, defineComponent, PropType, ref, watch } from "vue";
+import { computed, defineComponent, onBeforeUnmount, PropType, ref, watch } from "vue";
 import { newGuid } from "../Util/guid";
 import RockLabel from "./rockLabel";
 import { useFormState } from "../Util/form";
@@ -71,6 +71,7 @@ export default defineComponent({
         /** The internal value being tracked for the field. */
         const internalValue = ref<unknown>("");
 
+        /** The internal rules we will be used for calculations. */
         const internalRules = computed(() => normalizeRules(props.rules));
 
         /** Determines if this field is marked as required. */
@@ -109,6 +110,12 @@ export default defineComponent({
             }
         }, {
             immediate: true
+        });
+
+        // If we are removed from the DOM completely, clear the error before we go.
+        onBeforeUnmount(() => {
+            currentError.value = "";
+            formState?.setError(fieldLabel.value, "");
         });
 
         return {
